499163cc57754ff4cac4cbd3d4dc326b6c17a4f9
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2025-04-25 12:45:38 -0700

    hoist node links for pnpm

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -1,8 +1,19 @@
 const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
 
-const config = getDefaultConfig(__dirname);
+const projectRoot = __dirname;
+const monorepoRoot = path.resolve(projectRoot, '../..');
+
+const config = getDefaultConfig(projectRoot);
+
+config.watchFolders = [monorepoRoot];
 
 // Ensure we're only building for mobile platforms
 config.resolver.platforms = ['ios', 'android'];
+
+config.resolver.nodeModulesPaths = [
+  path.resolve(projectRoot, 'node_modules'),
+  path.resolve(monorepoRoot, 'node_modules'),
+];
 
 module.exports = config;
