b7c826f66f04d997f44fc2d932be2d4469d0e0ab
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2025-05-11 23:50:16 -0700

    fix og image (#11)

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -22,26 +22,31 @@ const oxygen = Oxygen({
   weight: ['400', '700'],
 })
 
-export const metadata: Metadata = {
-  title: 'TheraGPT - AI-Assisted CBT Journaling',
-  description:
-    'TheraGPT is an AI-powered journal that helps you quickly record and process negative thoughts with CBT techniques.',
-  openGraph: {
-    title: 'TheraGPT - AI-Assisted CBT Journaling',
+export const generateMetadata = async (): Promise<Metadata> => {
+  const isProd = process.env.NODE_ENV === 'production'
+  const baseUrl = isProd ? 'https://theragpt.ai' : 'http://localhost:3000'
+
+  return {
+    title: 'TheraGPT - AI-Assisted CBT Journal',
     description:
       'TheraGPT is an AI-powered journal that helps you quickly record and process negative thoughts with CBT techniques.',
-    siteName: 'TheraGPT',
-    images: [
-      {
-        url: '../public/assets/theragpt-social.png',
-        width: 1200,
-        height: 630,
-      },
-    ],
-    locale: 'en_US',
-    type: 'website',
-    url: 'https://theragpt.ai/',
-  },
+    openGraph: {
+      title: 'TheraGPT - AI-Assisted CBT Journal',
+      description:
+        'TheraGPT is an AI-powered journal that helps you quickly record and process negative thoughts with CBT techniques.',
+      siteName: 'TheraGPT',
+      images: [
+        {
+          url: `${baseUrl}/assets/theragpt-social.png`,
+          width: 1200,
+          height: 630,
+        },
+      ],
+      locale: 'en_US',
+      type: 'website',
+      url: baseUrl + '/',
+    },
+  }
 }
 
 export default function RootLayout({
