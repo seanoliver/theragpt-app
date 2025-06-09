a0e08d27630975992e0aef7e1f445d1099127011
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2025-06-08 17:27:02 -0700

    lint fix

--- packages/config/src/database.types.ts
+++ packages/config/src/database.types.ts
@@ -14,7 +14,7 @@ export type Database = {
           confidence_score: number | null
           created_at: string
           description: string
-          distortion_id: Database["public"]["Enums"]["distortion_type"]
+          distortion_id: Database['public']['Enums']['distortion_type']
           entry_id: string
           id: string
         }
@@ -22,7 +22,7 @@ export type Database = {
           confidence_score?: number | null
           created_at?: string
           description: string
-          distortion_id: Database["public"]["Enums"]["distortion_type"]
+          distortion_id: Database['public']['Enums']['distortion_type']
           entry_id: string
           id?: string
         }
@@ -30,24 +30,24 @@ export type Database = {
           confidence_score?: number | null
           created_at?: string
           description?: string
-          distortion_id?: Database["public"]["Enums"]["distortion_type"]
+          distortion_id?: Database['public']['Enums']['distortion_type']
           entry_id?: string
           id?: string
         }
         Relationships: [
           {
-            foreignKeyName: "distortion_instances_distortion_id_fkey"
-            columns: ["distortion_id"]
+            foreignKeyName: 'distortion_instances_distortion_id_fkey'
+            columns: ['distortion_id']
             isOneToOne: false
-            referencedRelation: "distortions"
-            referencedColumns: ["id"]
+            referencedRelation: 'distortions'
+            referencedColumns: ['id']
           },
           {
-            foreignKeyName: "distortion_instances_entry_id_fkey"
-            columns: ["entry_id"]
+            foreignKeyName: 'distortion_instances_entry_id_fkey'
+            columns: ['entry_id']
             isOneToOne: false
-            referencedRelation: "entries"
-            referencedColumns: ["id"]
+            referencedRelation: 'entries'
+            referencedColumns: ['id']
           },
         ]
       }
@@ -55,21 +55,21 @@ export type Database = {
         Row: {
           description: string
           examples: string[] | null
-          id: Database["public"]["Enums"]["distortion_type"]
+          id: Database['public']['Enums']['distortion_type']
           label: string
           strategies: string[] | null
         }
         Insert: {
           description: string
           examples?: string[] | null
-          id: Database["public"]["Enums"]["distortion_type"]
+          id: Database['public']['Enums']['distortion_type']
           label: string
           strategies?: string[] | null
         }
         Update: {
           description?: string
           examples?: string[] | null
-          id?: Database["public"]["Enums"]["distortion_type"]
+          id?: Database['public']['Enums']['distortion_type']
           label?: string
           strategies?: string[] | null
         }
@@ -135,11 +135,11 @@ export type Database = {
         }
         Relationships: [
           {
-            foreignKeyName: "reframes_entry_id_fkey"
-            columns: ["entry_id"]
+            foreignKeyName: 'reframes_entry_id_fkey'
+            columns: ['entry_id']
             isOneToOne: false
-            referencedRelation: "entries"
-            referencedColumns: ["id"]
+            referencedRelation: 'entries'
+            referencedColumns: ['id']
           },
         ]
       }
@@ -152,21 +152,21 @@ export type Database = {
     }
     Enums: {
       distortion_type:
-        | "all-or-nothing-thinking"
-        | "overgeneralization"
-        | "mental-filter"
-        | "disqualifying-the-positive"
-        | "jumping-to-conclusions"
-        | "magnification"
-        | "emotional-reasoning"
-        | "should-statements"
-        | "labeling"
-        | "personalization"
-        | "catastrophizing"
-        | "blaming"
-        | "fortune-telling"
-        | "mind-reading"
-        | "minimization"
+        | 'all-or-nothing-thinking'
+        | 'overgeneralization'
+        | 'mental-filter'
+        | 'disqualifying-the-positive'
+        | 'jumping-to-conclusions'
+        | 'magnification'
+        | 'emotional-reasoning'
+        | 'should-statements'
+        | 'labeling'
+        | 'personalization'
+        | 'catastrophizing'
+        | 'blaming'
+        | 'fortune-telling'
+        | 'mind-reading'
+        | 'minimization'
     }
     CompositeTypes: {
       [_ in never]: never
@@ -174,29 +174,29 @@ export type Database = {
   }
 }
 
-type DefaultSchema = Database[Extract<keyof Database, "public">]
+type DefaultSchema = Database[Extract<keyof Database, 'public'>]
 
 export type Tables<
   DefaultSchemaTableNameOrOptions extends
-    | keyof (DefaultSchema["Tables"] & DefaultSchema["Views"])
+    | keyof (DefaultSchema['Tables'] & DefaultSchema['Views'])
     | { schema: keyof Database },
   TableName extends DefaultSchemaTableNameOrOptions extends {
     schema: keyof Database
   }
-    ? keyof (Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"] &
-        Database[DefaultSchemaTableNameOrOptions["schema"]]["Views"])
+    ? keyof (Database[DefaultSchemaTableNameOrOptions['schema']]['Tables'] &
+        Database[DefaultSchemaTableNameOrOptions['schema']]['Views'])
     : never = never,
 > = DefaultSchemaTableNameOrOptions extends { schema: keyof Database }
-  ? (Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"] &
-      Database[DefaultSchemaTableNameOrOptions["schema"]]["Views"])[TableName] extends {
+  ? (Database[DefaultSchemaTableNameOrOptions['schema']]['Tables'] &
+      Database[DefaultSchemaTableNameOrOptions['schema']]['Views'])[TableName] extends {
       Row: infer R
     }
     ? R
     : never
-  : DefaultSchemaTableNameOrOptions extends keyof (DefaultSchema["Tables"] &
-        DefaultSchema["Views"])
-    ? (DefaultSchema["Tables"] &
-        DefaultSchema["Views"])[DefaultSchemaTableNameOrOptions] extends {
+  : DefaultSchemaTableNameOrOptions extends keyof (DefaultSchema['Tables'] &
+        DefaultSchema['Views'])
+    ? (DefaultSchema['Tables'] &
+        DefaultSchema['Views'])[DefaultSchemaTableNameOrOptions] extends {
         Row: infer R
       }
       ? R
@@ -205,21 +205,21 @@ export type Tables<
 
 export type TablesInsert<
   DefaultSchemaTableNameOrOptions extends
-    | keyof DefaultSchema["Tables"]
+    | keyof DefaultSchema['Tables']
     | { schema: keyof Database },
   TableName extends DefaultSchemaTableNameOrOptions extends {
     schema: keyof Database
   }
-    ? keyof Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"]
+    ? keyof Database[DefaultSchemaTableNameOrOptions['schema']]['Tables']
     : never = never,
 > = DefaultSchemaTableNameOrOptions extends { schema: keyof Database }
-  ? Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+  ? Database[DefaultSchemaTableNameOrOptions['schema']]['Tables'][TableName] extends {
       Insert: infer I
     }
     ? I
     : never
-  : DefaultSchemaTableNameOrOptions extends keyof DefaultSchema["Tables"]
-    ? DefaultSchema["Tables"][DefaultSchemaTableNameOrOptions] extends {
+  : DefaultSchemaTableNameOrOptions extends keyof DefaultSchema['Tables']
+    ? DefaultSchema['Tables'][DefaultSchemaTableNameOrOptions] extends {
         Insert: infer I
       }
       ? I
@@ -228,21 +228,21 @@ export type TablesInsert<
 
 export type TablesUpdate<
   DefaultSchemaTableNameOrOptions extends
-    | keyof DefaultSchema["Tables"]
+    | keyof DefaultSchema['Tables']
     | { schema: keyof Database },
   TableName extends DefaultSchemaTableNameOrOptions extends {
     schema: keyof Database
   }
-    ? keyof Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"]
+    ? keyof Database[DefaultSchemaTableNameOrOptions['schema']]['Tables']
     : never = never,
 > = DefaultSchemaTableNameOrOptions extends { schema: keyof Database }
-  ? Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+  ? Database[DefaultSchemaTableNameOrOptions['schema']]['Tables'][TableName] extends {
       Update: infer U
     }
     ? U
     : never
-  : DefaultSchemaTableNameOrOptions extends keyof DefaultSchema["Tables"]
-    ? DefaultSchema["Tables"][DefaultSchemaTableNameOrOptions] extends {
+  : DefaultSchemaTableNameOrOptions extends keyof DefaultSchema['Tables']
+    ? DefaultSchema['Tables'][DefaultSchemaTableNameOrOptions] extends {
         Update: infer U
       }
       ? U
@@ -251,53 +251,53 @@ export type TablesUpdate<
 
 export type Enums<
   DefaultSchemaEnumNameOrOptions extends
-    | keyof DefaultSchema["Enums"]
+    | keyof DefaultSchema['Enums']
     | { schema: keyof Database },
   EnumName extends DefaultSchemaEnumNameOrOptions extends {
     schema: keyof Database
   }
-    ? keyof Database[DefaultSchemaEnumNameOrOptions["schema"]]["Enums"]
+    ? keyof Database[DefaultSchemaEnumNameOrOptions['schema']]['Enums']
     : never = never,
 > = DefaultSchemaEnumNameOrOptions extends { schema: keyof Database }
-  ? Database[DefaultSchemaEnumNameOrOptions["schema"]]["Enums"][EnumName]
-  : DefaultSchemaEnumNameOrOptions extends keyof DefaultSchema["Enums"]
-    ? DefaultSchema["Enums"][DefaultSchemaEnumNameOrOptions]
+  ? Database[DefaultSchemaEnumNameOrOptions['schema']]['Enums'][EnumName]
+  : DefaultSchemaEnumNameOrOptions extends keyof DefaultSchema['Enums']
+    ? DefaultSchema['Enums'][DefaultSchemaEnumNameOrOptions]
     : never
 
 export type CompositeTypes<
   PublicCompositeTypeNameOrOptions extends
-    | keyof DefaultSchema["CompositeTypes"]
+    | keyof DefaultSchema['CompositeTypes']
     | { schema: keyof Database },
   CompositeTypeName extends PublicCompositeTypeNameOrOptions extends {
     schema: keyof Database
   }
-    ? keyof Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"]
+    ? keyof Database[PublicCompositeTypeNameOrOptions['schema']]['CompositeTypes']
     : never = never,
 > = PublicCompositeTypeNameOrOptions extends { schema: keyof Database }
-  ? Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"][CompositeTypeName]
-  : PublicCompositeTypeNameOrOptions extends keyof DefaultSchema["CompositeTypes"]
-    ? DefaultSchema["CompositeTypes"][PublicCompositeTypeNameOrOptions]
+  ? Database[PublicCompositeTypeNameOrOptions['schema']]['CompositeTypes'][CompositeTypeName]
+  : PublicCompositeTypeNameOrOptions extends keyof DefaultSchema['CompositeTypes']
+    ? DefaultSchema['CompositeTypes'][PublicCompositeTypeNameOrOptions]
     : never
 
 export const Constants = {
   public: {
     Enums: {
       distortion_type: [
-        "all-or-nothing-thinking",
-        "overgeneralization",
-        "mental-filter",
-        "disqualifying-the-positive",
-        "jumping-to-conclusions",
-        "magnification",
-        "emotional-reasoning",
-        "should-statements",
-        "labeling",
-        "personalization",
-        "catastrophizing",
-        "blaming",
-        "fortune-telling",
-        "mind-reading",
-        "minimization",
+        'all-or-nothing-thinking',
+        'overgeneralization',
+        'mental-filter',
+        'disqualifying-the-positive',
+        'jumping-to-conclusions',
+        'magnification',
+        'emotional-reasoning',
+        'should-statements',
+        'labeling',
+        'personalization',
+        'catastrophizing',
+        'blaming',
+        'fortune-telling',
+        'mind-reading',
+        'minimization',
       ],
     },
   },
